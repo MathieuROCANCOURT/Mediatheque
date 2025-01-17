a6666fcaa8458e86eb15ab8ba2efb751c4e69e82
Mathieu Rocancourt <mathieu.rocancourt@ensea.fr>
2025-01-17 14:52:00 +0100

    Add package important in CDList

--- resources/js/Components/CDList.tsx
+++ resources/js/Components/CDList.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import PrimaryButton from "./PrimaryButton";
+import {router} from '@inertiajs/react'; // Import Inertia router
 
 interface CD {
     id: number;
